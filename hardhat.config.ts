d3a4c7d650be6844a8c9af15a5ab4c9ae253c04c
Eric Lau <ericglau@outlook.com>
2022-10-19 10:26:41 -0400

    use optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  }
 };
 
 export default config;
